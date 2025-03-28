be78bcae7c4fca8df4963a5d37c4345ad80b11fb
bryrey <brysonreyes03@gmail.com>
2025-03-27 17:02:56 -0700

    Database.java outline

--- CougarArray/src/RecDatabase/Database.java
+++ CougarArray/src/RecDatabase/Database.java
@@ -5,5 +5,14 @@ package RecDatabase;
 
 //Subsystem
 public class database {
-    
+    /**
+     * +getUsers()
+     * Access the DB for a list of users, returns necessary info (name, IP, status, etc)
+     *
+     * +searchUser(userName)
+     * Used when running from commandline-- searching whether or not a user exists
+     *
+     * +getUserIP(userName)
+     * Takes username and returns actual IP, used to send the file in the end
+     */
 }
